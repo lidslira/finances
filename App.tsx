0d492cc63ca8fe01ff9b2ac1f6986cd7e0c8a08d
Ana Ribeiro <analidialirar@gmail.com>
2023-08-20 18:26:08 -0300

    ✨ feat: start register page

--- App.tsx
+++ App.tsx
@@ -2,15 +2,16 @@ import React from 'react';
 import AppLoading from 'expo-app-loading';
 import { ThemeProvider } from 'styled-components';
 
+import theme from './src/global/styles/theme';
+import Dashboard from './src/screens/Dashboard';
+import Register from './src/screens/Register';
+
 import {
   useFonts,
   Poppins_400Regular,
   Poppins_500Medium,
   Poppins_700Bold
 } from '@expo-google-fonts/poppins';
-
-import theme from './src/global/styles/theme';
-import Dashboard from './src/screens/Dashboard';
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -22,7 +23,7 @@ export default function App() {
 
   return (
     <ThemeProvider theme={theme}>
-      <Dashboard />
+      <Register />
     </ThemeProvider>
   );
 }
---
+++ src/screens/Register/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import * as S from './styles';
+
+const Register: React.FC = () => {
+  return (
+    <S.Container>
+      <S.Header>
+        <S.Title>Cadastro</S.Title>
+      </S.Header>
+
+    </S.Container>
+  );
+}
+
+export default Register;
